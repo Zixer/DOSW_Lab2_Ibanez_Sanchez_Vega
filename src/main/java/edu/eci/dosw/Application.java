833348c0c9cb6c5886a1b2d8a0b1ec4d9821e4a4
Sergio Vega <sergio.vega-p@mail.escuelaing.edu.co>
2026-08-12 20:14:38 -0500

    Initial Project Structure

---
+++ src/main/java/edu/eci/dosw/Application.java
@@ -0,0 +1,16 @@
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Maven project configured and running correctly.");
+
+        reto1DonPepeStore.run();
+        reto2FiveStarChef.run();
+        reto3KingdomofVehicles.run();
+        reto4CurrencyExchangeScam.run();
+        reto5CustomizedCoffed.run();
+        reto6TalktoTechnicalSupport.run();
+        reto7MagicRemoteControl.run();
+        reto8UMLZoo.run();
+
+    }
+}
