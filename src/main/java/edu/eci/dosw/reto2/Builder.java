52ab9c4089b499cc38f5cb67006a07b19ac0b71d
Yalixsan <yazidsa07@gmail.com>
2026-08-14 09:32:40 -0500

    feat(reto2): Implementation HamburguerBuilder & Builder Interface

---
+++ src/main/java/edu/eci/dosw/reto2/Builder.java
@@ -0,0 +1,18 @@
+public interface Builder {
+
+    Builder addBread();
+
+    Builder addMeat();
+
+    Builder addCheese();
+
+    Builder addLettuce();
+
+    Builder addTomato();
+
+    Builder addBacon();
+
+    Builder addBBQSauce();
+
+    Hamburger build();
+}
