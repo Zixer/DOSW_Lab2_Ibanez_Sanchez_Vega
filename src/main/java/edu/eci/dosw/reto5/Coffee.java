58571c0af2eb59737cd92db953b3eaf2b3599430
Sergio Vega <sergio.vega-p@mail.escuelaing.edu.co>
2026-08-16 03:34:52 -0500

    feat(reto5): Implement Coffe interface, Basic Coffe and Coffe Decorator

---
+++ src/main/java/edu/eci/dosw/reto5/Coffee.java
@@ -0,0 +1,24 @@
+package edu.eci.dosw.reto5;
+
+/* 
+Defines the common behavior for a coffee.*
+Both the basic coffee and all decorators
+implement this interface.
+*/
+public interface Coffee {
+
+/* 
+Returns the complete description of the coffee.
+@return coffee description
+*/
+
+String getDescription();
+
+/**
+Returns the total price of the coffee,
+including all added toppings.*
+@return total coffee price
+*/
+
+double getPrice();
+}
---
+++ src/main/java/edu/eci/dosw/reto5/CoffeeDecorator.java
@@ -0,0 +1,17 @@
+package edu.eci.dosw.reto5;
+
+/* 
+Base decorator for coffee objects.
+Stores a reference to another Coffee object
+so additional behavior can be added dynamically.
+*/
+
+public abstract class CoffeeDecorator implements Coffee {
+
+    protected final Coffee coffee;
+
+/*
+Creates a decorator that wraps another coffee.
+@param coffee coffee being decorated
+*/
+protected CoffeeDecorator(Coffee coffee) {this.coffee = coffee;}}
