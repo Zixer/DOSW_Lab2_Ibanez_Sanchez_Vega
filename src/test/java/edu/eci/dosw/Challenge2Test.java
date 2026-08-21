1f8994340910c3de59441d6fa7932faafe00f884
Sergio Vega <sergio.vega-p@mail.escuelaing.edu.co>
2026-08-20 23:01:21 -0500

    fix: tests class names

--- src/test/java/edu/eci/dosw/Challenge2Test.java
+++ src/test/java/edu/eci/dosw/Challenge2Test.java
@@ -12,7 +12,7 @@ import java.io.PrintStream;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Modifier;
 
-public class Reto2FiveStarChefTest {
+public class Challenge2Test {
 
     private final InputStream originalIn = System.in;
     private final PrintStream originalOut = System.out;
